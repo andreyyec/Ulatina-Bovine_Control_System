fa1a831e03a816cb6e296dcacc884ad68b1b09e1
Andrey Castro Carvajal <andreyyec@gmail.com>
2018-02-20 08:53:57 -0600

    [] Library Example consuming RF object

---
+++ libraries/RFManager/RFManager.h
@@ -0,0 +1,31 @@
+/*
+  RFManager.h - Library controlling RF Communications for the bovine necklace
+  Header file (it defines the class structure)
+*/
+#ifndef RFManager_h
+#define RFManager_h
+
+#include "SoftwareSerial.h"
+#include "Arduino.h"
+//#include "RF24.h"
+
+class RFManager {
+  public:
+    RFManager(RF24& radio);
+    void setWriter();
+    void setListener();
+    void sendData();
+    void getData();
+    void showData();
+    void updateMessage();
+  private:
+    const byte _slaveAddress[5] = {'R','x','A','A','A'};
+    char _dataReceived[11];
+    char _dataToSend[11];
+    bool _newData;
+    bool _rslt;
+    char _txNum = 1; 
+    RF24& _radio;
+};
+
+#endif
