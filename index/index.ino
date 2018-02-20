96d8728dc21242a680156c1ad6f3039b04321f0c
Andrey Castro Carvajal <andreyyec@gmail.com>
2018-02-20 08:52:41 -0600

    [] Added RF24 Library

--- index/index.ino
+++ index/index.ino
@@ -1,10 +1,19 @@
 #include <SoftwareSerial.h>
 #include <Test.h>
+#include <RF24.h>
+#include <RFManager.h>
+
+
+#define CE_PIN 22
+#define CSN_PIN 23
 
 Test testInstance(5);
 
+
 void setup() {
   Serial.begin(9600);
+  RF24 _radio(CE_PIN, CSN_PIN);
+  RFManager rfManagerInstance(_radio);
 }
 
 void loop() {
